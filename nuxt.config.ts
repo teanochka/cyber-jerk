ad0437bb271943334536b91ccc8407195e5133ae
teanochka <113801488+teanochka@users.noreply.github.com>
2026-02-17 02:15:52 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,8 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     gemini: {
-      apiKey: process.env.GEMINI_API_KEY || '',
-      model: process.env.GEMINI_MODEL || 'gemini-2.0-flash',
+      apiKey: '',
+      model: '',
     },
     public: {}
   }
